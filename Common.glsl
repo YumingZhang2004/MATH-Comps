b898cec973b497123f5487307b8548a2cfd4d0c9
enriqueza-cloud <enriqueza@carleton.edu>
2026-01-19 14:32:47 -0600

    new changed to smin, quadratic smooth instead of basic clamp

--- Common.glsl
+++ Common.glsl
@@ -37,10 +37,13 @@ struct Hit {
     float t;
     int id;
 };
+// Smooth minimum (quadratic polynomial smoothing)
+
 float smin( float a, float b, float k )
 {
-    float h = clamp( 0.5 + 0.5*(b - a)/k, 0.0, 1.0 );
-    return mix( b, a, h ) - k*h*(1.0 - h);
+    k *= 1.3;
+    float h = max( k-abs(a-b), 0.0 )/k;
+    return min(a,b) - h*h*k*(1.0/4.0);
 }
 
 const float INF = 1e6;
