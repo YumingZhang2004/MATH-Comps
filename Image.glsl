9871a8108a98cfb5e5e8df469042957d7de51aab
enriqueza-cloud <enriqueza@carleton.edu>
2026-02-19 12:21:43 -0600

    removed unused code, notated some elements
    Refactor scene definition and add new objects with smoothing.

--- Image.glsl
+++ Image.glsl
@@ -1,167 +1,93 @@
-// Choose n of D_{2n}
-const int D_N = 2; 
-
-const float M_1_PI = 1.0 / 3.1415926535897932384626433832795;
-const float M_1_2PI = 1.0 / 6.283185307179586476925286766559;
-
-vec2 wrapper(vec3 v_normal)
-{
-    vec3 n_normal = normalize(v_normal);
-    vec2 wrap;
-    wrap.x = - (0.5 - atan(n_normal.z, n_normal.x) * M_1_2PI);
-    wrap.y = (0.5 - asin(-n_normal.y) * M_1_PI);   
-    return wrap;
-}
-
-// Cell-local scene composition
-float cellSDF( in vec3 r, in float ksmooth )
-{
-    // Apply D_{2n} group action by folding into fundamental wedge
-    int dk, dref;
-    vec2 rzFold = Dn_fold(r.xz, D_N, dk, dref);
-    vec3 rf = vec3(rzFold.x, r.y, rzFold.y);
-    
-    // Example multi-object cell:
-    // main sphere
-    float dSph = sdSphere(rf - vec3(0.0, 0.0, 0.0), 0.7);
-
-    //  torus ring
-    float dTor = sdTorus(rf - vec3(0.0, 0.0, 0.0), vec2(1.1, 0.25));
-
-    //  small box pillar in the cell
-    float dBox = sdBox(rf - vec3(0.9, 0.0, 0.0), vec3(0.25, 0.25, 0.25));
-
-    // Smooth unions
-    float d = smin(dSph, dBox, ksmooth);
-
-    return d;
-}
-
 // Scene definition
-float mapp( in vec3 p )
+float sceneSDF( in vec3 p, out int matID)
 {
 
+    // Constant between 0.0-1.0 used to control strength of object smoothing
     float ksmooth = 0.3;
-  float s = max(600.0 - 200.0 * iTime, 3.0);
     
+    /* s: Determines the size of the domain. Ex. if s=3.0, then the fundamental domain 
+    is a 3x3x3 unit cube centered at the origin.*/
+    float s = max(600.0 - 200.0 * iTime, 6.0);
     //const float s = 4.;
-
-    const vec3 rep = vec3(1,1,1);
-
+    
+    /*rep: Only used when rendering limited repetition. Below, the fund. dom. is
+    duplicated once in both x directions, 2 in both y dirs, and 3 in both z dirs.*/
+    //const vec3 rep = vec3(3,1, 2);
     vec3 id = round(p/s);
-
     vec3 off = sign(p-s*id);
 
     float d = 1e20;
     for( int k=0; k<2; k++)
-    for( int j=0; j<2; j++ )
+    for( int j=0; j<2; j++)
     for( int i=0; i<2; i++ )
     {
         vec3 grid = id + vec3(i,j,k)*off;
-        grid = clamp(grid,-rep,rep); // limited repetition
-        vec3 r = p - s*grid;
         
-        // Replace single-object min() with multi-object scene
-        float dCell = cellSDF(r, ksmooth);
-        d = min(d, dCell);
+        // Uncomment below for limited repetition
+        //grid = clamp(grid,-rep,rep); 
+        vec3 p = p - s*grid;        
         
-        float xdir = sin(10.*  iTime);    
-        float zdir = -cos(10.* iTime);
-        //float s1 = sdSphere(r - vec3(0.,0.,0.), .4);
-        //float s1 = sdSphere(r - vec3(xdir, -xdir * .2, zdir), .7);
-        //float s1 = sdSphere(p - vec3(0.,heightjump,0.), 0.4);
-        //float d1 = smin(d, s1, k);
+        // Use move in the position vector to move the sdf in that direction without glitching out.
+        //float move = mod(20. * iTime + 0.5 *s, s) - 0.5*s;
+        float sphere = sdSphere(p, 0.8);
+        
+        // extra object to add. May impact performance.
+        float torus = sdTorus(p - vec3(0.0, -0.2, 0.0), vec2(1.2, 0.1));
+        float sphere2 = sdSphere(p - vec3(0.4, 1.0, 0.3), 0.3);
+        float sphere3 = sdSphere(p- vec3(-0.4, 1.4, -1.0), 0.3);
+        float box = sdBoxFrame(p - vec3(0.0, 4.0, 0.0), vec3(0.5, 1.0,  0.5), 0.035);
 
-        float left = mod(20. * iTime + 0.5 *s, s) - 0.5*s;
-        float sphere = sdSphere(r - vec3(0.,0.,0.), .7);
-
-        //float torus = sdTorus(r - vec3(0.,0.0,0.), vec2(1.4,.5));
-        //float cell = smin(sphere, torus, ksmooth);
-        d = min(d, sphere);
-        //d = min(d, torus);
-        //d = smin(d, cell, ksmooth);
+    
+    if (sphere < d){
+        d = sphere;
+        matID = 0;
+    }
+    if (torus < d){
+        d = torus;
+        matID = 1;
+    }
+    if (sphere2 < d){
+        d = sphere2;
+        matID = 2;
+    }
+    if (sphere3 < d){
+        d = sphere3;
+        matID = 3;
+    }    
+    if (box < d){
+        d = box;
+        matID = 4;
+    }
+    
+    //float cell = smin(sphere, torus, ksmooth);
+    //d = smin(d, cell, ksmooth);
 
     }
     
     return d;
 }    
 
-
-/*float sceneSDF(vec3 p, out int matID)
-{
-    
-    
-    
-    float k = 0.7;
-    matID = -1;
-    p.xz = mod(p.xz + 1.0, 2.0 ) - 1.0;  
-    float heightjump = 0.2 + 0.2*sin(5. * iTime);
-
-    float d = sdPlane(p, vec3(0.,0.,0.), 1.0);
-    matID = 0;
-
-    float s1 = sdSphere(p - vec3(heightjump,0.,0.), 0.2);
-    float d1 = smin(d, s1, k);
-    if (d1 != d) matID = 1;
-    d = d1;
-    
-
-
-    
-    return d;
-    
-}*/
-
-// Wrapper used by soft shadows
-/*float map(vec3 p)
-{
-    int _;
-    return sceneSDF(p, _);
-}*/
-
-// Geometry utilities
+// Calculates normal at p
 vec3 getNormal(vec3 p)
 {
     vec2 e = vec2(0.001, 0.0);
+    int _;
     return normalize(vec3(
-        mapp(p + e.xyy) - mapp(p - e.xyy),
-        mapp(p + e.yxy) - mapp(p - e.yxy),
-        mapp(p + e.yyx) - mapp(p - e.yyx)
+        sceneSDF(p + e.xyy, _) - sceneSDF(p - e.xyy, _),
+        sceneSDF(p + e.yxy, _) - sceneSDF(p - e.yxy, _),
+        sceneSDF(p + e.yyx, _) - sceneSDF(p - e.yyx, _)
     ));
 }
 
-// Soft shadows
-/*float calcSoftshadow(in vec3 ro, in vec3 rd,
-                     in float mint, in float tmax,
-                     in float w)
-{
-    float res = 1.0;
-    float t   = mint;
-    float ph  = 1e10;
-
-    for(int i=0; i<32; i++)
-    {
-        float h = mapp(ro + rd*t);
-        float y = h*h/(2.0*ph);
-        float d = sqrt(max(h*h - y*y, 0.0));
-        res = min(res, d/(w*max(0.0, t - y)));
-        ph = h;
-
-
-        t += h;
-        if(res < 0.0001 || t > tmax) break;
-    }
-
-    res = clamp(res, 0.0, 1.0);
-    return res*res*(3.0 - 2.0*res);
-}*/
+// Creates smooth shadows with gradient penumbra
 float softshadow( in vec3 ro, in vec3 rd, float mint, float maxt, float k )
 {
     float res = 1.0;
     float t = mint;
     for( int i=0; i<256 && t<maxt; i++ )
     {
-        float h = mapp(ro + rd*t);
+        int _;
+        float h = sceneSDF(ro + rd*t, _);
         if( h<0.001 )
             return 0.0;
         res = min( res, k*h/t );
@@ -170,16 +96,15 @@ float softshadow( in vec3 ro, in vec3 rd, float mint, float maxt, float k )
     return res;
 }
 
-
 // Lighting (Phong + shadow factor)
 vec3 calculateLighting(vec3 p, vec3 normal, vec3 viewDir,
                        vec3 lightPos, vec3 lightColor,
                        float sha)
 {
-    float ambientStrength  = .25;
-    float diffuseStrength  = .95;
-    float specularStrength = .45;
-    float shininess        = 0.;
+    float ambientStrength  = 0.25;
+    float diffuseStrength  = 0.95;
+    float specularStrength = 0.45;
+    float shininess = 0.0;
 
     // Ambient
     vec3 ambient = ambientStrength * lightColor;
@@ -198,28 +123,26 @@ vec3 calculateLighting(vec3 p, vec3 normal, vec3 viewDir,
 }
 
 // Raymarcher
+// Note: lower render distance for performance (t > 1000 is rather high)
 vec3 raymarch(vec3 ro, vec3 rd, out int matID)
 {
-    float t = 0.0;
-    matID = -1;
-
-    for(int i=0; i<2560; i++)
+    float t;
+    
+    for(int i=0; i<550; i++)
     {
         vec3 p = (ro + rd*t);
-
-        // int stepID;
-        float d = mapp(p);
+        int stepID;
+        float d = sceneSDF(p, stepID);
 
         // Hit
         if(d < 0.001)
         {
-            // matID = stepID;
-            matID = 0;
+            matID = stepID;
             return p;
         }
 
         t += d;
-        if(t > 500.0) break;
+        if(t > 1000.0) break;
     }
 
     matID = -1;
@@ -239,7 +162,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord)
     camPose[3] = texelFetch(iChannel1, ivec2(3,0), 0);
 
     mat3 camRot = mat3(camPose);
-    vec3 camPos = camPose[3].xyz - vec3(0.,0.0,-.75);
+    vec3 camPos = camPose[3].xyz - vec3(0.0,0.0,-0.85);
 
     vec3 ro = camPos;
     vec3 rd = normalize(camRot * normalize(vec3(uv, 1.5)));
@@ -250,42 +173,42 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord)
 
     // Background
     if(matID < 0)
-    {
-        //vec3 sky = vec3(0.6, 0.8, 1.0) * (1.0 - 0.5 * rd.y);
+    {        
         vec3 sky = vec3(0.0);
         fragColor = vec4(sky, 1.0);
         return;
     }
 
     // Material base color
-    vec3 colors[4];
-    colors[0] = vec3(2., 1., .4);
-    colors[1] = vec3(1.,1.,1.);
+    // If adding more objects, update colors here
+    vec3 colors[5];
+    colors[0] = vec3(0.4, 0.1, 0.9);
+    colors[1] = vec3(0.1, 0.2, 0.6);
     colors[2] = vec3(0.1, 0.6, 0.4);
     colors[3] = vec3(0.5, 0.5, 0.3);
+    colors[4] = vec3(0.7, 0.7, 0.0);
     vec3 baseColor = colors[matID];
 
     vec3 normal  = getNormal(p);
     vec3 viewDir = normalize(ro - p);
 
+    // Stationary light source
     //vec3 lightPos   = vec3(0., 1., 0.);
+    
+    // Light follows camera
     vec3 lightPos = ro - vec3(0.0, 0.5, 0.0);
     vec3 lightColor = vec3(1.0);
     
-    vec2 wrap = wrapper(normal);
-    vec3 texCol = texture(iChannel2, wrap).xyz;
 
-    baseColor = texCol;  
     // Soft shadow factor
     vec3 lightDir = normalize(lightPos - p);
     float dist = length(p - lightPos);
-    float fade = 3.0/ (1.0 + dist * dist * 0.2) + .12;
-    float sha = softshadow(p, lightDir, .12, dist, 205.);
+    float fade = 2.0/(1.0 + dist * dist * 0.2) + 2.0;
+    float sha = softshadow(p, lightDir, .12, dist, 200.0);
 
     // Lighting
     vec3 lit = calculateLighting(p, normal, viewDir, lightPos, lightColor, sha);
 
     vec3 col = baseColor * lit * fade;
     fragColor = vec4(col, 1.0);
-
-
+}
